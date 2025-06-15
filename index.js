25f5e9060fbcc279456dcaff5c7dc57d58c115ad
jtf-charles <charlesjosephtancredefils2001@gmail.com>
2025-06-14 23:08:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const tutorials = [
+  'what does the this keyword mean?',
+  'What is the Constructor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
+
+const titleCased = () => {
+  const titles=tutorials.map(title =>title.split(' '))
+  const capitalizedTitles = titles.map(title => {
+    return title.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+  })
+  return capitalizedTitles 
+}
